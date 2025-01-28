201e2f2dd230e9170dd00ca7723af34df2bc96d0
Slashbull <67091409+Slashbull@users.noreply.github.com>
2025-01-28 14:07:56 +0530

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,10 @@
-
+# importer_dashboard/core/__init__.py
+# This allows: from core import load_and_preprocess_data, authenticate_user, logout_user
+# Instead of: from core.core import load_and_preprocess_data, etc.
+from .core import load_and_preprocess_data
+from .security import authenticate_user, logout_user
+__all__ = [
+    "load_and_preprocess_data",
+    "authenticate_user",
+    "logout_user",
+]
